b3ad1ae68e28264d707fbee1f0f8b12eb1cc5127
CatsAreMLG <joshuadgon@gmail.com>
2019-04-24 23:21:28 -0500

    Add Procfile

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+}
