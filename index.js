8437fd6f8fc64accee69fffa5d60ca97f1b1eba0
CatsAreMLG <joshuadgon@gmail.com>
2019-04-24 21:03:45 -0500

    Add helmet and .env exception

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const server = express()
+const helmet = require('helmet')
 require('dotenv').config()
 
 const PORT = process.env.PORT_ENV || 5000
 
+server.use(helmet())
 server.use(express.json())
 
 server.get('/', (req, res) => res.status(200).json({ api: 'up' }))
