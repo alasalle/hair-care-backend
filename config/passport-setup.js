93cfc6bbd349610aa129a732d2c704a13fc2ad0d
CatsAreMLG <joshuadgon@gmail.com>
2019-04-27 14:52:48 -0500

    Fix google strategy function

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,12 +3,15 @@ const GoogleStrategy = require('passport-google-oauth20')
 require('dotenv')
 
 passport.use(
-  new GoogleStrategy({
-    // options for google strategy
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-  }),
-  _ => {
-    // passport callback function
-  }
+  new GoogleStrategy(
+    {
+      // options for google strategy
+      callbackURL: '/auth/google/redirect',
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET
+    },
+    _ => {
+      // passport callback function
+    }
+  )
 )
