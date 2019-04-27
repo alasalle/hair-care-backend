7cf8722b6c0e13bc5f858d92d2f5d4160e44d71a
CatsAreMLG <joshuadgon@gmail.com>
2019-04-27 14:47:06 -0500

    Add initial passportjs setup

---
+++ config/passport-setup.js
@@ -0,0 +1,14 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+require('dotenv')
+
+passport.use(
+  new GoogleStrategy({
+    // options for google strategy
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  }),
+  _ => {
+    // passport callback function
+  }
+)
